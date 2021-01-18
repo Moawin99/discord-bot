c5a0c5a44922732e8a649d1ef064f4b709aef27b
Mark Moawad <msmoawad99@gmail.com>
2021-01-17 21:59:50 -0500

    changed api to zenquotes

--- cogs/text/text.py
+++ cogs/text/text.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import requests
+import json
 
 class Text(commands.Cog):
     def __init__(self, bot):
@@ -19,9 +20,10 @@ class Text(commands.Cog):
 
     @commands.command(brief="--gives good advice!")
     async def advice(self, ctx):
-        advice = requests.get('https://api.adviceslip.com/advice')
-        data = advice.json()
-        await ctx.send(data)
+        response = requests.get("https://zenquotes.io/api/random")
+        data = json.loads(response.text)
+        quote = data[0]['q'] + " -" + data[0]['a']
+        await ctx.send(quote)
         
 
 def setup(bot):
